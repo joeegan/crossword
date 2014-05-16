d639f73a94afb64fb22d6e397158994f6f5a0206
joe <joe@joeegan.co.uk>
2014-05-16 21:59:36 +0100

    initialise with one of 4 starting grids

--- src/crossword.js
+++ src/crossword.js
@@ -5,35 +5,13 @@ class Crossword {
       this.trs = this.tableEl.querySelectorAll('tr');
       this.size = this.trs.length;
       this.minimumWordLength = 4;
-      this.validWordLengths = this.getValidWordLengths();
       return this;
    }
 
    initialise() {
-      this.colourMapping = this.merge(this.buildColourMapping(), this.buildColourMapping());
+      this.colourMapping = this.buildInitialGrid();
       this.colourBoard();
    }
-
-   /**
-    * Returns a multi array where multiArrayB has been turned on it's side and merged into the multiArrayA.
-    * @returns {{[[]]}}
-    */
-   merge(multiArrayA:Array, multiArrayB:Array) {
-      var innerLength = multiArrayA.length;
-      var arr = new Array(innerLength);
-      for (var i = 0; i < innerLength; i++) {
-         arr[i] = [];
-         for (var j =0; j < innerLength; j++) {
-            arr[i][j] = [];
-            if(multiArrayA[i][j] || multiArrayB[j][i]) {
-               arr[i][j] = 1;
-            } else {
-               arr[i][j] = 0;
-            }
-         }
-      }
-      return arr;
-   };
 
    /**
     * Colours the board black and white using the colourMapping.
@@ -42,66 +20,11 @@ class Crossword {
       for (let i = 0; i < this.colourMapping.length; i++) {
          var tds = this.tableEl.querySelectorAll('tr')[i].querySelectorAll('td');
          for (let j = 0; j < tds.length; j++) {
-            if (!this.colourMapping[i][j]) {
+            if (this.colourMapping[i][j] == 0) {
                tds[j].className = 'blank';
             }
          }
       }
-   };
-
-   /**
-    * Decides which word lengths to use on the board.
-    * Ensures rotational symmetry is adhered to.
-    */
-   buildColourMapping() {
-      var topHalf = [];
-      var startWhite = this.getRandomInt(0,1);
-      var i = startWhite;
-      var len = Math.floor(this.size/2) + i;
-      for (i; i < len; i++) {
-         if (i % 2 == 0) {
-            topHalf.push(new Array(this.size))
-         } else {
-            topHalf.push(this.buildLineArray());
-         }
-      }
-      var middleRow = startWhite ? this.buildSymmetricalLineArray() : new Array(this.size);
-      var secondSection = [for (row of topHalf) row.slice().reverse()];
-      topHalf.push(middleRow);
-      return topHalf.concat(secondSection.reverse());
-   };
-
-   /**
-    * @returns {Number[]} e.g [1,1,1,1,0,1,1,1,1,1,1,1];
-    */
-   buildLineArray() {
-      var lineArr = [];
-      var firstWordLength = this.getRandomWordLength();
-
-      var lineArr = Array.apply(null, Array(firstWordLength)).map( s => 1 );
-
-      if (firstWordLength !== this.size) {
-         lineArr.push(0);
-      }
-      var secondWordLength = this.size - 1 - firstWordLength;
-      for (let i = 0; i < secondWordLength; i++) {
-         lineArr.push(1);
-      }
-      return lineArr;
-   };
-
-   buildSymmetricalLineArray() {
-      var wordLength = this.getRandomWordLength();
-      var lineArr = [];
-      var halfLength = Math.floor(this.size/2);
-      for (let i = 0; i < halfLength; i++) {
-         if (i < wordLength) {
-            lineArr.push(1);
-         } else {
-            lineArr.push(0);
-         }
-      }
-      return lineArr.concat(this.getRandomInt(0,1)).concat(lineArr.slice().reverse());
    };
 
    /**
@@ -111,18 +34,35 @@ class Crossword {
       return Math.floor(Math.random() * (max - min + 1)) + min;
    };
 
-   getRandomWordLength(arr = this.validWordLengths) {
-      return arr[this.getRandomInt(0, arr.length - 1)];
-   };
-
-   getValidWordLengths() {
+   buildInitialGrid() {
       var arr = [];
-      for (var i = this.minimumWordLength - 1; i < (this.size - 3); i++) {
-         arr.push(i);
-      }
-      return arr;
-   };
+      var lightRow = new String(new Array(13)).split(',').map(function(){
+         return 1
+      });
+      var altRowStartDark = lightRow.map(function(num, i) {
+         return +(i % 2 == 0)
+      });
+      var altRowStartLight = altRowStartDark.map(function(num) {
+         return Math.abs(num-1)
+      });
+      var choices = [lightRow, altRowStartDark, altRowStartLight];
 
+      var firstChoiceIndex = this.getRandomInt(0, choices.length-1);
+      var firstChoice = choices.splice(firstChoiceIndex, 1)[0];
+      var secondChoice, secondChoiceIndex;
+      if (firstChoice.toString() !== lightRow.toString()) {
+         secondChoice = lightRow;
+      } else {
+         secondChoiceIndex = this.getRandomInt(0, choices.length-1);
+         secondChoice = choices.splice(secondChoiceIndex, 1)[0];
+      }
+
+      while (arr.length < this.size) {
+         arr.push(firstChoice);
+         arr.push(secondChoice);
+      }
+      return arr.splice(0, this.size);
+   }
 }
 
 var crossword = new Crossword('table').initialise();
